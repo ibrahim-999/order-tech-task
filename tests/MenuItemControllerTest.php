e27b3604988bd6dac188ee64a4c5a4283da22d6c
⁨Ibrahim Khalaf⁩ <ikhalaf@devsolutions.team>
2024-03-12 17:17:14 +0200

    [add] unit test scenario for upload function

---
+++ tests/MenuItemControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests;
+
+use App\Http\Controllers\Api\v1\MenuItemController;
+use App\Http\Services\ImportServices;
+use App\Http\Services\MenuService;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Mockery;
+
+class MenuItemControllerTest extends TestCase
+{
+    protected $importService;
+    protected $menuService;
+    protected $menuItemController;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->importService = Mockery::mock(ImportServices::class);
+        $this->menuService = Mockery::mock(MenuService::class);
+
+        $this->menuItemController = new MenuItemController($this->importService, $this->menuService);
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+
+        parent::tearDown();
+    }
+
+    public function testUpload()
+    {
+        $request = Mockery::mock(Request::class);
+        $file = 'file.json'; // you can adjust the extension you want
+        $path = public_path('/files');
+
+        $this->importService->shouldReceive('uploadFileData')
+            ->once()
+            ->with($path, $file);
+
+        $response = $this->menuItemController->upload($request);
+
+        $this->assertEquals(['message' => 'File has been uploaded successfully'], $response);
+    }
+
+}
